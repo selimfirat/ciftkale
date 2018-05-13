83924966ff542bfbc1a6607f438b11fda35b17df
PythEch <pythech.tr@gmail.com>
2018-05-14 00:15:05 +0300

    coca cola zero

--- ciftkale/pageUtils.py
+++ ciftkale/pageUtils.py
@@ -149,7 +149,7 @@ def getPlayerInfo(username):
             'assist' : assist,
             'shot' : shot,
             'yellow_card' : yellow_card,
-            'red_card' : red_card
+            'red_card' : red_card,
             'result' : 'success'
         }
 
