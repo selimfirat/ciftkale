77932cd4ca0371d76632cd50d595e2f513474a6e
Selim Fırat Yılmaz <yilmazselimfirat@gmail.com>
2018-05-13 23:39:01 +0300

    Update settings.py

--- ciftkale/settings.py
+++ ciftkale/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
