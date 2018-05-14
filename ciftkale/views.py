5e1389f433e37719427d95a3254cd12eb1691f74
Alper Şahıstan <alpersahistan@gmail.com>
2018-05-15 00:10:07 +0300

    2 end-points added

--- ciftkale/views.py
+++ ciftkale/views.py
@@ -187,3 +187,16 @@ def players_view(request):
   response = getPlayersTable(filterTeam, filterCountry, filterPlayer, filterAgent, sortQuery, pageSize, page)
   return JsonResponse(response)
 
+@csrf_exempt
+def player_view(request):
+    username  = request.GET['username']
+
+    response = getPlayerInfo(username)
+    return JsonResponse(response)
+
+@csrf_exempt
+def coach_view(request):
+     username  = request.GET['username']
+
+    response = getCoachInfo(username)
+    return JsonResponse(response)
